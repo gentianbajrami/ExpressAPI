3470d66cd106b2fe6054ce2db673f0412560cf38
Gentian Bajrami <gentian.bajrami20@outlook.com>
2022-06-14 15:07:51 +0200

    Create friends.model.js

---
+++ models/friends.model.js
@@ -0,0 +1,16 @@
+const friends = [
+    {
+      id: 0,
+      name: "Albert Einstein",
+    },
+    {
+      id: 1,
+      name: "Isaac Newton",
+    },
+    {
+      id: 2,
+      name: "Elon Musk",
+    },
+  ];
+
+  module.exports = friends;
